1609fc3a5386a7b6c9e8cd896cb9bb9121e1b7a7
oggy95 <gorobchuk95@gmail.com>
2022-07-10 12:03:16 +0300

    v1 dz

---
+++ contacts.js
@@ -0,0 +1,77 @@
+import fs from "fs/promises";
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const filePath = `${__dirname}\\db\\contacts.json`;
+
+/**
+ * List contacts from base
+ * @return {Array} array of contacts
+ */
+async function listContacts() {
+    const result = await fs.readFile(filePath);
+    return JSON.parse(result);
+}
+
+/**
+ * Get contact by ID
+ * @param {Number} contactId Contact ID
+ * @return {Object|null} Contact
+ */
+async function getContactById(contactId) {
+    const contacts = await listContacts();
+    const result = contacts.find(item => parseInt(item.id) === contactId);
+    return result ? result : null;
+}
+
+/**
+ * Remove contact by ID
+ * @param {Number} contactId Contact ID
+ * @return {Object|null} Contact
+ */
+async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(item => parseInt(item.id) === contactId);
+    if (idx === -1) {
+        return null;
+    }
+    const [result] = contacts.splice(idx, 1);
+    await updateContacts(contacts);
+    return result;
+}
+
+/**
+ * Update database
+ * @param {Array} contacts new array to update
+ */
+async function updateContacts(contacts) {
+    await fs.writeFile(filePath, JSON.stringify(contacts, null, 2));
+}
+
+/**
+ * Insert new contact into database
+ * @param {string} name Name of contact
+ * @param {string} email Email of contact
+ * @param {string} phone Phone number of contact
+ */
+async function addContact(name, email, phone) {
+    const contacts = await listContacts();
+    const id = (contacts.length + 1).toString();
+    const insertObject = {
+        id,
+        name,
+        email,
+        phone
+    };
+    contacts.push(insertObject);
+    await updateContacts(contacts);
+    return contacts;
+}
+
+export default {
+    getContactById,
+    addContact,
+    removeContact,
+    listContacts
+}
---
+++ index.js
@@ -0,0 +1,54 @@
+import contacts from "./contacts.js";
+import yargs from "yargs";
+import {hideBin} from "yargs/helpers";
+
+async function testContactsLib() {
+    try {
+        const listOfContacts = await contacts.listContacts();
+        console.log(listOfContacts);
+
+        const contact = await contacts.getContactById(2);
+        console.log("Contact:", contact);
+
+        const new_contacts = await contacts.addContact("Berlard", "berlards@gmail.com", "+401567843");
+        console.log("New list", new_contacts);
+
+        const removed_contact = await contacts.removeContact(12);
+        console.log("Removed contact", removed_contact);
+    } catch (e) {
+        console.error("Error spawned. Details:", e.toString());
+    }
+
+}
+
+async function invokeAction({action, id, name, email, phone}) {
+    try {
+        switch (action) {
+            case "list":
+                console.log(await contacts.listContacts());
+                break;
+
+            case "get":
+                console.log(await contacts.getContactById(id));
+                break;
+
+            case "add":
+                console.log(await contacts.addContact(name, email, phone));
+                break;
+
+            case "remove":
+                console.log(await contacts.removeContact(id));
+                break;
+
+            default:
+                console.warn("\x1B[31m Unknown action type!");
+        }
+    } catch (e) {
+        console.error("Error spawned. Details:", e.toString());
+    }
+
+}
+
+const arr = hideBin(process.argv);
+const {argv} = yargs(arr);
+invokeAction(argv)
